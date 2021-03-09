1f33014c6cbb3fd6016c22a4932c3ffb1e0e6fd1
Renato de Azevedo Lacerda <renatolacerda@matur.com.br>
2021-03-09 14:45:44 -0300

    Adicionar arquivos de projeto.

---
+++ imagemtopdf/Program.cs
@@ -0,0 +1,28 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+
+namespace imagemtopdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            criapdf();
+        }
+        public static void criapdf() {
+            PdfDocument doc = new PdfDocument();
+            doc.Info.Title = "Renato lindo";
+            PdfPage page = doc.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+            gfx.DrawString("Renato é Lindo", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            string path = @"C:\Users\renatolacerda\Desktop\tesseract\lol.pdf";
+            doc.Save(path);
+        }
+        public static void imgTopdf()
+        {
+                
+        }
+    }
+}
